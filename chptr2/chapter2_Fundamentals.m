c7149b9403355cbce1c4b7359d4be0ce89f23dbf
Ajay Gunalan <ajay.gunalan@iit.it>
2021-12-03 04:44:36 +0100

    binarize success

--- chptr2/chapter2_Fundamentals.m
+++ chptr2/chapter2_Fundamentals.m
@@ -9,17 +9,25 @@ whos f
 %% 2.4 Writing Images
 imwrite(f, 'camNew.jpg')
 %% 2.5 Data Classes
-s = class(f)
+s = class(f);
 %% 2.6 Image types
 A = [1, 0; 0, 1];
 islogical(A);
 B = logical(A);
 islogical(B);
 %% 2.7 Converting between data classes
-c = double(f);
-class(c);
-f = [-0.5, 0.5; 0.75, 1.5];
-g = im2uint8(f)
+%c = double(f);
+%class(c);
+%f = [-0.5, 0.5; 0.75, 1.5];
+%g = im2uint8(f);
+%h = uint8([25 50; 128 200]);
+%g = im2double(h);
+g = imbinarize(f);
+g1 = g(:,:,1);
+imshow(g1)
+
+%imshow(g1)
+
 
 %% 2.8 Array Indexing
 %% 2.9 Intro to MATLAB
