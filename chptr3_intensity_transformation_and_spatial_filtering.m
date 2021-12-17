904feac7e84a80a3698da76c3bef27dda3d2d8ab
Ajay Gunalan <ajay.gunalan@iit.it>
2021-12-17 11:41:50 +0100

    Done: Histogram Processing; TODO: 3.4 & 3.5: Linear & Nonlinear Spatial Filtering

--- chptr3_intensity_transformation_and_spatial_filtering.m
+++ chptr3_intensity_transformation_and_spatial_filtering.m
@@ -42,7 +42,47 @@ idx = find(X >= 3 & X < 6);
 [r, c] = find(X >= 3 & X < 6);
 disp([r,c]);
 intensityScaling
-%% 3.3 Historam Processing
+%% 3.3 Histogram Processing
+f = imread('liver_cells.jpg');
+f = rgb2gray(f);
+%figure, imshow(f);
+h = imhist(f, 30)/numel(f)
+horz = linspace(0, 255, 30);
+figure, bar(horz, h, 'FaceColor', [0 106 78]/255, 'EdgeColor', [0 212 156]/255, 'LineWidth', 0.75);
+ax = gca;
+ax.Color = [190 228 223]/255;
+ax.YTick = 0:max(h(:))/4:max(h(:));
+ax.FontName = 'Times Ten';
+ax.FontSize = 8;
+%% Histogram Equalization
+f = imread('flower.jpeg');
+figure, imshow(f);
+g = histeq(f, 256);
+figure, imshow(g);
+figure, imhist(f);
+figure, imhist(g);
+hnorm = imhist(f)/numel(f);
+cdf = cumsum(hnorm);
+r = linspace(0,1,256);
+figure, plot(r, cdf);
+%% Histogram Matching
+f = im2double(imread('chestXray.jpg'));
+figure, imshow(f);
+load goldenChestXrayHistogram
+figure, plot(f, goldenChestXrayHistogram);
+%% Adaptive Histogram Equalization
+f = imread('hidden-symbols.tif');
+figure, imshow(f); 
+fe = histeq(f, 100);
+figure, imshow(fe);
+g = adapthisteq(f,'ClipLimit',0.4,'Distribution','exponential','NumTiles',[12 12]);
+figure, imshow(g);
+
+
+
+
+
+
 %% 3.4 Linear Spatial Filtering
 %% 3.5 Nonlinear Spatial Filtering
 %% 3.6 Using Fuzzy Sets for Intensity Transformations and Spatial Filtering
