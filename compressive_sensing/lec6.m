fe31ccd1c9041df141db52da0bb385677a4a5415
Ajay Gunalan <ajay.gunalan@iit.it>
2022-04-21 11:51:19 +0200

    added lec 7

--- compressive_sensing/lec6.m
+++ compressive_sensing/lec6.m
@@ -1,4 +1,4 @@
-clear all, close all, clc
+clear all, close all, clc;
 
 %% Solve y = theta * s for "s"
 n = 1000;   % dimension for S
@@ -18,7 +18,6 @@ cvx_end;
 s_L2 = pinv(Theta)*y;
 
 %% Plot
-
 figure
 subplot(3,2,1);
 plot(s_L1, 'r', 'LineWidth', 1.5)
@@ -30,5 +29,14 @@ plot(s_L2, 'c', 'LineWidth', 1.5)
 title('L2 solution', 'Color', 'k', 'FontSize', 24)
 ylim([-.2 .2]), grid on
 
+% Histogram
 subplot(3,2,[3 5]);
-[hc,h] =
+[hc,h] = hist(s_L1, [-0.1:0.01:0.1]);
+bar(h, hc, 'r');
+axis([-0.1 0.1 -50 1000]);
+
+subplot(3, 2, [4 6]);
+[hc,h] = hist(s_L2, [-0.1:0.01:0.1]);
+bar(h, hc, 'c');
+axis([-0.1 0.1 -20 400]);
+% set(gcf, 'Position', [1500 100 1800 1000]);
