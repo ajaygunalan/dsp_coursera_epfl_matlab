52d641ef8a23f35778b1675f76b3aa625da6992a
Ajay Gunalan <ajay.gunalan@iit.it>
2022-08-01 16:19:15 +0200

    spiralSamp fucn created

---
+++ sampler/spiralSamp.m
@@ -0,0 +1,13 @@
+function [x y] = spiralSamp(Image, nSpirals, nPoints, dSpirals, bPlot)
+    [height width] = size(Image);
+    theta = linspace(0,360*nSpirals, nPoints);
+    x = round(((width/2)+1) +(theta/dSpirals).*cosd(theta));
+    y = round(((height/2)+1) +(theta/dSpirals).*sind(theta));
+    if (bPlot)
+        imshow(Image);
+        hold on;
+        plot(x,y);
+        axis([0 width 0 height]);
+        grid on;
+    end
+end
